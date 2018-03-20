02488529b568cf4e94e26245b3fb1f5c360bc7e1
Youngtrust <36457348+Youngtrust@users.noreply.github.com>
2018-03-20 14:03:17 -0400

    Create HomeController.cs

---
+++ How to handle multi submit buttons/HomeController.cs
@@ -0,0 +1,7 @@
+[HttpPost]
+[MultipleButton(Name = "action", Argument = "Save")]
+public ActionResult Save(MessageModel mm) { ... }
+
+[HttpPost]
+[MultipleButton(Name = "action", Argument = "Cancel")]
+public ActionResult Cancel(MessageModel mm) { ... }
